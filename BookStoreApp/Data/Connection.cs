8282a96e742066192eb96d60247f8d9818306d72
KAMBA <ik2032@hw.ac.uk>
2024-07-17 18:38:12 +0100

    Add project files.

---
+++ BookStoreApp/Data/Connection.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace BookStoreApp.Data
+{
+    class Connection
+    {
+        public static MySqlConnection conn = new MySqlConnection();
+
+        static string server = "sql8.freesqldatabase.com;";
+        static string database = "sql8720437;";
+        static string Uid = "sql8720437;";
+        static string password = "SWBRJQR7PI;";
+
+        public static MySqlConnection dataSource()
+        {
+            conn = new MySqlConnection($"server={server} database={database} Uid={Uid} password={password}");
+            return conn;
+        }
+
+        public void connOpen()
+        {
+            dataSource();
+            conn.Open();
+        }
+
+        public void connClose()
+        {
+            dataSource();
+            conn.Close();
+        }
+    }
+}
---
+++ BookStoreApp/Form1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BookStoreApp
+{
+    public partial class Form1 : Form
+    {
+        Data.Connection con = new Data.Connection();
+        public Form1()
+        {
+            InitializeComponent();
+            Data.Connection.dataSource();
+            con.connOpen();
+        }
+    }
+}
