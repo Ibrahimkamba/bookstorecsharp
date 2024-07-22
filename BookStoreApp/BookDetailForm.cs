9829a84d986b2cf17987c7d3e2a1c778f3a470a8
KAMBA <ik2032@hw.ac.uk>
2024-07-22 17:26:25 +0100

    correct book details view

--- BookStoreApp/BookDetailForm.cs
+++ BookStoreApp/BookDetailForm.cs
@@ -1,11 +1,16 @@
 ﻿using System;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace BookStoreApp
 {
     public partial class BookDetailForm : Form
     {
         private string bookId;
+        private static readonly string server = "bkbxqvr3u99gdowtsu8c-mysql.services.clever-cloud.com";
+        private static readonly string database = "bkbxqvr3u99gdowtsu8c";
+        private static readonly string uid = "u5dmf8qxsxlp6sca";
+        private static readonly string password = "iMsCpRUikhByXlmWSQnb";
 
         public BookDetailForm(string bookId)
         {
@@ -16,9 +21,48 @@ namespace BookStoreApp
 
         private void DisplayBookDetails()
         {
-            // Mock book details
-            string bookDetails = $"Book ID: {bookId}\nTitle: Example Book\nAuthor: John Doe\nSubject: Programming\nPublished: 2023";
-            richTextBox1.Text = bookDetails;
+            string connectionString = $"server={server};database={database};uid={uid};password={password};";
+            string query = @"SELECT B.B_TITLE, CONCAT(A.A_FNAME, ' ',A.A_LNAME) AS A_NAME, B.B_SUBJECT, B.B_PUB_DATE 
+                             FROM Books B 
+                             JOIN Author A ON B.B_A_ID = A.A_ID 
+                             WHERE B.B_ID = @BookId";
+
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                MySqlCommand command = new MySqlCommand(query, connection);
+                command.Parameters.AddWithValue("@BookId", bookId);
+
+                try
+                {
+                    connection.Open();
+                    using (MySqlDataReader reader = command.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            string title = reader["B_TITLE"].ToString();
+                            string author = reader["A_NAME"].ToString();
+                            string subject = reader["B_SUBJECT"].ToString();
+                            string published = reader["B_PUB_DATE"].ToString();
+
+                            string bookDetails = $"Book ID: {bookId}\nTitle: {title}\nAuthor: {author}\nSubject: {subject}\nPublished: {published}";
+                            richTextBox1.Text = bookDetails;
+                        }
+                        else
+                        {
+                            MessageBox.Show("Book not found.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            this.Close();
+                        }
+                    }
+                }
+                catch (MySqlException ex)
+                {
+                    MessageBox.Show($"MySQL error occurred: {ex.Message}", "Database Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"An unexpected error occurred: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
         }
 
         private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
